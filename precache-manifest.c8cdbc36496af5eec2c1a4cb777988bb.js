7ae1812c303758418f799cc5b0c2889843bcea0a
joerverson santos <joerverson.santos@gmail.com>
2020-09-23 21:31:08 -0300

    adjustin security

---
+++ precache-manifest.c8cdbc36496af5eec2c1a4cb777988bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37cb0be728f3d46ef054b067ec143bbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14b9a0dc5302558bba97",
+    "url": "/static/css/main.24d84b3c.chunk.css"
+  },
+  {
+    "revision": "46980879689fd5f5c75d",
+    "url": "/static/js/2.b7e58b46.chunk.js"
+  },
+  {
+    "revision": "8c78fa0ccfa4a42789228b83d7c476e2",
+    "url": "/static/js/2.b7e58b46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b9a0dc5302558bba97",
+    "url": "/static/js/main.ed59adb6.chunk.js"
+  },
+  {
+    "revision": "0a942fe4f0f82543b7ca",
+    "url": "/static/js/runtime-main.81e4275d.js"
+  }
+]);
